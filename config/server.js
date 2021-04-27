1b0d26108deef50ccec1fbea52e5274469fa3199
BrendenTogioka <brendentogioka@gmail.com>
2021-04-26 19:53:47 -0700

    First Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '4f71af4850fb48bdf56362dbf3c7eb2f'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1ae43595-476a-4006-a12a-f31f68844d38'
+};
